43827c17093c401086825d5d6639303132294552
Nitish Kumar <nitishkumargenius@gmail.com>
2020-10-08 02:14:30 +0530

    Added test method for sorting Algorithms, fixed the bug in merge sort

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './SortingVisualizer.css';
-import * as SortingAlgorithms from  './sortingAlgorithms';
+import * as SortingAlgorithms from  './sortingAlgorithms/sortingAlgorithms';
 
 export default class SortingVisualizer extends React.Component{
 
@@ -25,7 +25,10 @@ export default class SortingVisualizer extends React.Component{
     }
 
     mergeSort() {
+        const javaScriptSortedArray = this.state.array.slice().sort((a, b) => a-b);
         const sortedArray = SortingAlgorithms.mergeSort(this.state.array);
+
+        console.log(arrayAreEqual(javaScriptSortedArray, sortedArray));
     }
 
     //Todo: Implement these also
@@ -34,6 +37,22 @@ export default class SortingVisualizer extends React.Component{
     // heapSort() {}
 
     // bubbleSort() {}
+
+    //function to test sorting algorithms
+    testSortingAlgorithms(){
+        for(let i=0; i<100; i++){
+            const array = [];
+            const length = randomIntFromInterval(1, 1000);
+            
+            for(let i=0; i<length; i++){
+                array.push(randomIntFromInterval(-1000, 1000));
+            }
+            const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
+            
+            const mergeSortedArray = SortingAlgorithms.mergeSort(array.slice());
+            console.log(arrayAreEqual(javaScriptSortedArray, mergeSortedArray));
+        }
+    }
 
     render(){
         const {array} = this.state;
@@ -44,16 +63,20 @@ export default class SortingVisualizer extends React.Component{
               <div
                 className="array-bar"
                 key={index}
-                style={{ height: `${value}px`, }}
-            ></div>
+                style={{ height: `${value}px` }}
+              ></div>
             ))}
-            <button onClick={() => this.resetArray()}>Generate New Array</button>
+            <button onClick={() => this.resetArray()}>
+              Generate New Array
+            </button>
             <button onClick={() => this.mergeSort()}>Merge Sort</button>
             {/*
             <button onClick={() => this.resetArray()}>Generate New Array</button>
             <button onClick={() => this.resetArray()}>Generate New Array</button>
             <button onClick={() => this.resetArray()}>Generate New Array</button> 
             */}
+            <button onClick={() => this.mergeSort()}>Merge Sort</button>
+            <button onClick={() => this.testSortingAlgorithms()}>Test Sorting Algorithms</button>
           </div>
         );
     };
@@ -63,6 +86,14 @@ export default class SortingVisualizer extends React.Component{
 function randomIntFromInterval(min, max){
     //min and max included
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function arrayAreEqual(arrayOne, arrayTwo){
+    if(arrayOne.length !== arrayTwo.length) return false;
+    for(let i=0; i<arrayOne.length; i++){
+        if(arrayOne[i] !== arrayTwo[i]) return false;
+    }
+    return true;
 }
 
 // export default SortingVisualizer;
--- src/components/sortingAlgorithms/sortingAlgorithms.js
+++ src/components/sortingAlgorithms/sortingAlgorithms.js
@@ -1,5 +1,5 @@
 export const mergeSort = (array) => {
-    if(array.length == 1) return array;
+    if(array.length === 1) return array;
 
     const middleIndex = Math.floor(array.length/2);
     const firstHalf = mergeSort(array.slice(0, middleIndex));
@@ -14,11 +14,11 @@ export const mergeSort = (array) => {
             sortedArray.push(firstHalf[i++]);
         }
         else{
-            sortedArray.push(secondHalf[j]);
+            sortedArray.push(secondHalf[j++]);
         }
     }
 
     while(i < firstHalf.length) sortedArray.push(firstHalf[i++]);
     while(j < secondHalf.length) sortedArray.push(secondHalf[j++]);
     return sortedArray; 
-}
+};
