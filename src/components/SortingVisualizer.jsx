d91eca41eebbc2992479846d0db8d99496930735
Nitish Kumar <nitishkumargenius@gmail.com>
2020-10-09 23:49:45 +0530

    Implemented merge Sort funcationality and animation

--- src/components/SortingVisualizer.jsx
+++ src/components/SortingVisualizer.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import './SortingVisualizer.css';
-import * as SortingAlgorithms from  './sortingAlgorithms/sortingAlgorithms';
+//import * as SortingAlgorithms from  './sortingAlgorithms/sortingAlgorithms';
+import {getMergeSortAnimations} from './sortingAlgorithms/Mergesort';
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 310;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = 'blue';
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = 'red';
 
 export default class SortingVisualizer extends React.Component{
 
@@ -18,17 +31,36 @@ export default class SortingVisualizer extends React.Component{
 
     resetArray(){
         const array = [];
-        for(let i=0; i<310; i++){
+        for(let i=0; i<NUMBER_OF_ARRAY_BARS; i++){
             array.push(randomIntFromInterval(5, 730));
         }
         this.setState({array});
     }
 
     mergeSort() {
-        const javaScriptSortedArray = this.state.array.slice().sort((a, b) => a-b);
-        const sortedArray = SortingAlgorithms.mergeSort(this.state.array);
-
-        console.log(arrayAreEqual(javaScriptSortedArray, sortedArray));
+        const animations = getMergeSortAnimations(this.state.array);
+        
+        for(let i=0; i<animations.length; i++){
+            const arrayBars = document.getElementsByClassName('array-bar');
+            const isColorChange = i % 3 !== 2;
+            if(isColorChange){
+                const[barOneIdx, barTwoIdx] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            }
+            else{
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
+        }
     }
 
     //Todo: Implement these also
@@ -49,7 +81,7 @@ export default class SortingVisualizer extends React.Component{
             }
             const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
             
-            const mergeSortedArray = SortingAlgorithms.mergeSort(array.slice());
+            const mergeSortedArray = getMergeSortAnimations(array.slice());
             console.log(arrayAreEqual(javaScriptSortedArray, mergeSortedArray));
         }
     }
@@ -70,12 +102,13 @@ export default class SortingVisualizer extends React.Component{
               Generate New Array
             </button>
             <button onClick={() => this.mergeSort()}>Merge Sort</button>
-            {/*
-            <button onClick={() => this.resetArray()}>Generate New Array</button>
-            <button onClick={() => this.resetArray()}>Generate New Array</button>
-            <button onClick={() => this.resetArray()}>Generate New Array</button> 
+            {/* Todo: 
+            <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            <button onClick={() => this.insertionSort()}>Insertion Sort</button>
+            <button onClick={() => this.selectionSort()}>Selection Sort</button> 
+            <button onClick={() => this.quickSort()}>Quick Sort</button> 
+            <button onClick={() => this.heapSort()}>Heap Sort Sort</button> 
             */}
-            <button onClick={() => this.mergeSort()}>Merge Sort</button>
             <button onClick={() => this.testSortingAlgorithms()}>Test Sorting Algorithms</button>
           </div>
         );
